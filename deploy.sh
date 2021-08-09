ad9594cb5ea76d27188aeb378acb81bbbe214c95
Filipe <filipefreire@protonmail.com>
2021-08-09 20:34:14 +0200

    -fix: path in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git commit -m 'deploy'
 
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:filipe-freire/svelte-social-reviews-example.git main:gh-pages
+git push -f git@github.com:filipe-freire/svelte-social-reviews-example.git main:gh-pages
 
 cd -
